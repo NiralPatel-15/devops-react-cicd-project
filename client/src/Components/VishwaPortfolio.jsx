4ef265126b25c4fa11a46fb277c57b7c205ae2f1
Niral Patel <niralpatel1502@gmail.com>
2026-03-15 19:13:10 +0530

    Updated Vishwa portfolio component

--- client/src/Components/VishwaPortfolio.jsx
+++ client/src/Components/VishwaPortfolio.jsx
@@ -1,32 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const VishwaPortfolio = () => {
   return (
-    <div style={{
-      border: "2px solid #2196F3",
-      padding: "20px",
-      margin: "20px",
-      borderRadius: "10px",
-      backgroundColor: "#f0f8ff"
-    }}>
-      <h2>Vishwa Tandel 👩‍💻</h2>
-      <p><b>DevOps Project Collaborator</b></p>
-
-      <h3>Skills</h3>
-      <ul>
-        <li>Git Collaboration</li>
-        <li>Docker Containers</li>
-        <li>CI/CD Workflow</li>
-        <li>React Development</li>
-      </ul>
-
-      <h3>Project Contribution</h3>
-      <p>
-        Collaborated in building the DevOps project, contributed to application
-        development and deployment using GitHub and Docker.
-      </p>
+    <div>
+      
     </div>
-  )
-}
+  );
+};
 
-export default VishwaPortfolio
+export default VishwaPortfolio;
